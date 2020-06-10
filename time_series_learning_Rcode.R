a4c5773753e199f9d1402aa6b71ec7e55f9b4bd5
Brian Pan <brianpanzifan@gmail.com>
2020-06-10 15:07:49 -0400

    update

--- time_series_learning_Rcode.R
+++ time_series_learning_Rcode.R
@@ -12,3 +12,9 @@ my_data = read_csv( "ontario_marriages.csv" )
 toronto_data = my_data %>% 
   filter(YEAR > 1980, CITY == "TORONTO" ) %>% 
   arrange( YEAR, MONTH ) 
+
+marriages = ts( toronto_data$COUNT, start=c(1980,1), frequency=12)
+plot(marriages, type='o', pch=20)
+acf(marriages)
+# Use the ```window``` function to extract a subset of the series from Jan 1990 till Dec 2000, and plot it.
+window( marriages, start = c(1990,1), end = c(2000, 12) )
