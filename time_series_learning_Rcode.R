af5a65a18711a34c8c5ec16a514e2da99abf5c6c
Brian Pan <brianpanzifan@gmail.com>
2020-07-02 02:36:09 -0400

    updatew

--- time_series_learning_Rcode.R
+++ time_series_learning_Rcode.R
@@ -490,3 +490,12 @@ for(i in 1:N){
   beta[i] = (lm(Y~X))$coef[2]
 }
 hist(beta)
+
+n=100
+beta = rep(0,N)
+for(i in 1:N){
+  X = cumsum(rnorm(n))
+  W = rnorm(n)
+  beta[i] = (lm(W~X))$coef[2]
+}
+hist(beta)
