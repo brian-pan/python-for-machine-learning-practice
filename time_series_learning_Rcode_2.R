403ef45d249cbc9246bd82b3c9463039c746e30f
Brian Pan <brianpanzifan@gmail.com>
2020-08-10 22:54:04 -0400

    update

--- time_series_learning_Rcode_2.R
+++ time_series_learning_Rcode_2.R
@@ -8,3 +8,10 @@ n = 100; h = 1:n - 1
 X = cumsum( rnorm(n))
 acf( X, demean = FALSE, type = "covariance", lag.max = n-1 )
 lines( h, (n-h) * (n-h+1) / 2 / n, col = 2 )
+
+N=1000
+acf.mat = matrix(0,N,n)
+for(i in 1:N){
+  Xt = cumsum( rnorm(n))
+  acf.mat[i,] = acf( Xt, demean = FALSE, type = "covariance", lag.max = n-1, plot = FALSE )$acf
+}
