bb98b09e6cd34dea553244c568982f764baf52ae
Brian Pan <brianpanzifan@gmail.com>
2020-08-11 14:37:26 -0400

    1

--- time_series_learning_Rcode_2.R
+++ time_series_learning_Rcode_2.R
@@ -15,3 +15,8 @@ for(i in 1:N){
   Xt = cumsum( rnorm(n))
   acf.mat[i,] = acf( Xt, demean = FALSE, type = "covariance", lag.max = n-1, plot = FALSE )$acf
 }
+plot( h, colMeans(acf.mat), type = "h", xlab = "mean ACF" )
+lines( h, (n-h) * (n-h+1) / n / 2, col = 2 )
+lines( h, colMeans(acf.mat) + 2*apply(acf.mat, 2, sd) / sqrt(N), col = 3)
+lines( h, colMeans(acf.mat) - 2*apply(acf.mat, 2, sd) / sqrt(N), col = 3)
+legend( "topright", legend = c("theoretical", "95% CI"), col = 2:3, lwd=2 )
