5ea70fe4bdac3fded75fc89da5d8b25a43d77224
Brian Pan <brianpanzifan@gmail.com>
2020-08-17 15:24:54 -0400

    1

--- time_series_learning_Rcode_3.R
+++ time_series_learning_Rcode_3.R
@@ -6,3 +6,8 @@ library(vars)
 library(forecast)
 library(Metrics)
 library(tseries)
+
+# get unadjusted (raw) series
+ua = get_cansim_vector( "v2057814", start_time = "1980-01-01", end_time = "1999-12-01") %>%
+  pull(VALUE) %>% 
+  ts( start = c(1980,1), frequency = 12)
