a7eefa64a7fd9b9eed0d1cd50f11d2476a9f2fed
Brian Pan <brianpanzifan@gmail.com>
2020-09-01 14:53:43 -0400

    2

--- time_series_learning_Rcode_3.R
+++ time_series_learning_Rcode_3.R
@@ -187,3 +187,9 @@ plot(gdpfit$residuals)
 acf(gdpfit$residuals)
 qqnorm(gdpfit$residuals)
 qqline(gdpfit$residuals)
+
+x11_method = seasadj(fit_1)
+auto.arima(gdp, xreg=x11_method, ic="aic")
+reggdp=sarima(gdp, 0,1,1,2,0,0,12, xreg = x11_method)
+reggdp
+mae(gdpres,na.omit(reggdp$fit$residuals))
